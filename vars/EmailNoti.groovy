64a637694bd0c6068ef00cd0e4e31feed8e79779
Zhe Ying <83914599+Pikaciu@users.noreply.github.com>
2024-05-06 15:15:29 +0800

    Update EmailNoti.groovy

--- vars/EmailNoti.groovy
+++ vars/EmailNoti.groovy
@@ -1,5 +1,5 @@
 // email Notification
-def EmailNoti(stage,appName,email_recipient,version){
+def EmailNoti(String stage, String appName, String email_recipient, String version){
     emailext(subject: """${stage} Status [Started] - Application: ${appName} ${version} [Staging] BUILD : ${env.BUILD_NUMBER}""", mimeType: 'text/html', body: """
     <html>
     <head>
